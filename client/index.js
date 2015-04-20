f232bc94b00b92c1e8fb33ae3a684384e4f3829b
Jon Edwards <jkedwards@me.com>
2015-04-20 15:32:48 -0700

    add list item fun function

--- client/index.js
+++ client/index.js
@@ -15,16 +15,44 @@ angular.module('alpha', [])
     var color = $scope.newColor;
     console.info('you want to add the color', color);
     $scope.colors.push(color);
-  }
+  };
   $scope.removeColor = function(index){
     console.warn('you just clicked remove color', index);
     $scope.colors.splice(index, 1);
-  }
+  };
 }])
 .controller('beta', ['$scope', function($scope){
   console.info('beta controller initialized');
   $scope.x = 3;
   $scope.y = 2;
+}])
+.controller('listfun', ['$scope', function($scope){
+  console.info('listfun controller initialized');
+  $scope.compute = function(string){
+    var array = string.split(',');
+    array = array.map(function(n){
+      return parseInt(n);
+    });
+    var sum = 0, product = 1;
+    array.forEach(function(n){
+      sum += n;
+      product *= n;
+    });
+    $scope.sum = sum;
+    $scope.product = product;
+    $scope.array = array;
+  };
+  $scope.colorOfNumber = function(num){
+    return (num%2) ? 'red' : 'blue';
+  };
+  $scope.removeListItem = function(index){
+    $scope.array.splice(index, 1);
+    $scope.compute($scope.array.join(','));
+  };
+  $scope.addNum = function(newNum){
+    $scope.array.push(newNum);
+    $scope.compute($scope.array.join(','));
+  };
 }])
 .run(['$rootScope', function($rootScope){
   console.info('running the run function');
